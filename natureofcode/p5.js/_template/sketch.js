be4823f47b441fa88fd731e0bbc0c6fbd60054f5
Andy Tumelty <andrew.tumelty@gmail.com>
2021-01-25 18:25:28 +0000

    Add p5.js template folder

---
+++ natureofcode/p5.js/_template/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    var canvas = createCanvas(800, 600, WEBGL);
+    canvas.parent('sketch');
+    // a slightly nicer starting offset
+    cam_s = 4.3;
+    cam_t = 5;
+}
+
+function draw() {
+    background(253);
+    lights();
+    drawCamera(true);
+
+    // _template
+
+    if (frameCount % 30 == 0){
+        document.getElementById('framerate').innerText = frameRate().toFixed(2);
+    }
+}
